b7c39e4e2f6fa2a8f68bdb55f1691cf52deec6d8
LeahWi <Leah0934@gmail.com>
2016-01-20 16:43:58 +0200

    More fixes to slider

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-﻿var app = angular.module('slider', ['ui.bootstrap', 'dialogs.main'])
+﻿var app = angular.module('slider', ['ngAnimate','ui.bootstrap', 'dialogs.main'])
 	.config(['dialogsProvider', function (dialogsProvider) {
 	    dialogsProvider.useBackdrop('static');
 	    dialogsProvider.useEscClose(false);
